45002a125f137e3a0ee011dc878d54cf671bd697
Tatiana Osipova <otv120780@gmail.com>
2022-12-07 10:37:46 -0500

    feat: add map

--- js/main.js
+++ js/main.js
@@ -8,5 +8,15 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-right',
         prevEl: '.swiper-button-left',
     },
-
 });
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
