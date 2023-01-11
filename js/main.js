eff390b3708e9e89a6370b28997dadd25dd80672
Tatiana Osipova <otv120780@gmail.com>
2023-01-11 11:58:23 -0500

    feat: add file js to menu-mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 2,
@@ -13,136 +21,120 @@ const swiper = new Swiper('.swiper', {
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.23187521487724, lng: -8.62807927972108 },
-    zoom: 14,
-    styles: [
-        {
-            "featureType": "administrative",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": "-100"
-                }
-            ]
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 37.23187521487724,
+            lng: -8.62807927972108
         },
-        {
-            "featureType": "administrative.province",
-            "elementType": "all",
-            "stylers": [
-                {
+        zoom: 14,
+        styles: [{
+                "featureType": "administrative",
+                "elementType": "all",
+                "stylers": [{
+                    "saturation": "-100"
+                }]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "all",
+                "stylers": [{
                     "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": -100
-                },
-                {
-                    "lightness": 65
-                },
-                {
-                    "visibility": "on"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": -100
-                },
-                {
-                    "lightness": "50"
-                },
-                {
-                    "visibility": "simplified"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "all",
-            "stylers": [
-                {
+                }]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [{
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 65
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [{
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": "50"
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [{
                     "saturation": "-100"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "all",
-            "stylers": [
-                {
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [{
                     "visibility": "simplified"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "all",
-            "stylers": [
-                {
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [{
                     "lightness": "30"
-                }
-            ]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "all",
-            "stylers": [
-                {
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [{
                     "lightness": "40"
-                }
-            ]
-        },
-        {
-            "featureType": "transit",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": -100
-                },
-                {
-                    "visibility": "simplified"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "hue": "#ffff00"
-                },
-                {
-                    "lightness": -25
-                },
-                {
-                    "saturation": -97
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "labels",
-            "stylers": [
-                {
-                    "lightness": -25
-                },
-                {
-                    "saturation": -100
-                }
-            ]
-        }
-    ]
-  });
+                }]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [{
+                        "saturation": -100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                        "hue": "#ffff00"
+                    },
+                    {
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -97
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [{
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -100
+                    }
+                ]
+            }
+        ]
+    });
 }
 
 window.initMap = initMap;
-
